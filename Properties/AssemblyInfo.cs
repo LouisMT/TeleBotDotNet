c9e9e77dda60e0e0c6580716d9fd27ecb1293bcf
Louis Matthijssen <louis@ict-crew.nl>
2015-09-27 17:25:50 +0200

    Update version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2015.9.6.1")]
-[assembly: AssemblyFileVersion("2015.9.6.1")]
+[assembly: AssemblyVersion("2015.9.18.1")]
+[assembly: AssemblyFileVersion("2015.9.18.1")]
