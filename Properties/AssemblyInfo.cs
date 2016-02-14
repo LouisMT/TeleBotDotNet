aec73beb5fb3856f2c2ad737758ed3f2436fbab8
Louis Matthijssen <louis@u5r.nl>
2016-02-14 20:55:15 +0100

    Bump version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2016.02.14.1")]
-[assembly: AssemblyFileVersion("2016.02.14.1")]
+[assembly: AssemblyVersion("2016.02.14.2")]
+[assembly: AssemblyFileVersion("2016.02.14.2")]
